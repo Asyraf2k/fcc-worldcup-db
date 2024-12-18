3dfafd4dca8e4b773299366496e8d3187616b7cc
Asyraf  Febriansyah <afebry444@gmail.com>
2024-12-18 09:21:52 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+if [[ $1 == "test" ]]
+then
+  DB_CONNECTION="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  DB_CONNECTION="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
 
-echo $($PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY")
 
+echo $($DB_CONNECTION "TRUNCATE teams, games")
 
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+cat games.csv | while IFS="," read GAME_YEAR GAME_ROUND WINNING_TEAM OPPONENT_TEAM WINNER_SCORE OPPONENT_SCORE
 do
-
-  if [[ $YEAR != "year" ]]
+  if [[ $GAME_YEAR != "year" ]]
   then
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    if [[ -z $WINNER_ID ]]
+    WINNER_TEAM_ID=$($DB_CONNECTION "SELECT team_id FROM teams WHERE name='$WINNING_TEAM'")
+
+    if [[ -z $WINNER_TEAM_ID ]]
     then
-      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+      ADD_WINNER_RESULT=$($DB_CONNECTION "INSERT INTO teams(name) VALUES('$WINNING_TEAM')")
+      if [[ $ADD_WINNER_RESULT == "INSERT 0 1" ]]
       then
-        echo "Tim ditambahkan: $WINNER"
+        echo "Tim pemenang ditambahkan: $WINNING_TEAM"
       fi
-      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     fi
 
-    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    if [[ -z $OPPONENT_ID ]]
+    OPPONENT_TEAM_ID=$($DB_CONNECTION "SELECT team_id FROM teams WHERE name='$OPPONENT_TEAM'")
+
+    if [[ -z $OPPONENT_TEAM_ID ]]
     then
-      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+      ADD_OPPONENT_RESULT=$($DB_CONNECTION "INSERT INTO teams(name) VALUES('$OPPONENT_TEAM')")
+      if [[ $ADD_OPPONENT_RESULT == "INSERT 0 1" ]]
       then
-        echo "Tim ditambahkan: $OPPONENT"
+        echo "Tim lawan ditambahkan: $OPPONENT_TEAM"
       fi
-      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     fi
 
-    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
-    if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+    WINNER_TEAM_ID=$($DB_CONNECTION "SELECT team_id FROM teams WHERE name='$WINNING_TEAM'")
+    OPPONENT_TEAM_ID=$($DB_CONNECTION "SELECT team_id FROM teams WHERE name='$OPPONENT_TEAM'")
+
+    # Menambahkan pertandingan ke tabel games
+    ADD_GAME_RESULT=$($DB_CONNECTION "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) 
+                                     VALUES($GAME_YEAR, '$GAME_ROUND', $WINNER_TEAM_ID, $OPPONENT_TEAM_ID, $WINNER_SCORE, $OPPONENT_SCORE)")
+    if [[ $ADD_GAME_RESULT == "INSERT 0 1" ]]
     then
-      echo "Pertandingan ditambahkan: $YEAR $ROUND - $WINNER vs $OPPONENT"
+      echo "Pertandingan ditambahkan: $GAME_YEAR $GAME_ROUND - $WINNING_TEAM vs $OPPONENT_TEAM"
     fi
   fi
 done
